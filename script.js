f5320cf7aea4a0d3a4074811b58d5d202747525c
iminlikewithyou <christian@omg.games>
2023-10-22 07:30:41 -0500

    update frontend

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 async function processImage() {
-  const form = new FormData(document.getElementById('imageForm'));
+  const fileInput = document.getElementById('fileInput');
+  const colorCountInput = document.getElementById('colorCount').value;
+  const dimensionInput = document.getElementById('dimension').value;
+
+  const formData = new FormData();
+  formData.append('image', fileInput.files[0]);
+  formData.append('colorCount', colorCountInput);
+  formData.append('dimension', dimensionInput);
 
   const response = await fetch('http://localhost:3000/processImage', {
     method: 'POST',
-    body: form,
+    body: formData,
   });
 
-  const data = await response.json();
-  console.log(data.colors);
+  const result = await response.json();
+
+  // Display color palette
+  const paletteDiv = document.getElementById('palette');
+  paletteDiv.innerHTML = '';
+  result.colors.forEach((color) => {
+    const colorDiv = document.createElement('div');
+    colorDiv.style.width = '20px';
+    colorDiv.style.height = '20px';
+    colorDiv.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+    paletteDiv.appendChild(colorDiv);
+  });
+
+  // Display the new image
+  const newImageElement = document.getElementById('newImage');
+  newImageElement.src = `data:image/png;base64,${result.newImage}`;
 }
+
